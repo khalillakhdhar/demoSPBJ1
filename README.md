9e74cc3e268746e7f76f0af08523b448d7f24089
khalil <khalillakhdharatc@gmail.com>
2023-03-29 09:17:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demoSPBJ1
+ 
---
+++ src/main/java/com/demo/DemoSpbj1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpbj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpbj1Application.class, args);
+	}
+
+}
